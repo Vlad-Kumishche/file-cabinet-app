c2f792fbf63ab2ebf0c7bd7d35d00ec7e773ae50
Uladzislau Kumishcha <mottedli@ya.ru>
2022-02-05 17:08:50 +0300

    Implemented the "Chain of Responsibilities" template for commands.

---
+++ FileCabinetApp/CommandHandlers/AppCommandRequest.cs
@@ -0,0 +1,35 @@
+﻿namespace FileCabinetApp.CommandHandlers
+{
+    /// <summary>
+    /// Application command request.
+    /// </summary>
+    public class AppCommandRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppCommandRequest"/> class.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="parameters">The command parameters.</param>
+        public AppCommandRequest(string command, string parameters)
+        {
+            this.Command = command;
+            this.Parameters = parameters;
+        }
+
+        /// <summary>
+        /// Gets the command.
+        /// </summary>
+        /// <value>
+        /// The command.
+        /// </value>
+        public string Command { get; }
+
+        /// <summary>
+        /// Gets the command parameters.
+        /// </summary>
+        /// <value>
+        /// The command parameters.
+        /// </value>
+        public string Parameters { get; }
+    }
+}
---
+++ FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
@@ -0,0 +1,19 @@
+﻿namespace FileCabinetApp.CommandHandlers
+{
+    /// <summary>
+    /// CommandHandlerBase.
+    /// </summary>
+    public abstract class CommandHandlerBase : ICommandHandler
+    {
+        private ICommandHandler? nextHandler;
+
+        /// <inheritdoc/>
+        public abstract void Handle(AppCommandRequest appCommandRequest);
+
+        /// <inheritdoc/>
+        public void SetNext(ICommandHandler commandHandler)
+        {
+            this.nextHandler = commandHandler ?? throw new ArgumentNullException(nameof(commandHandler));
+        }
+    }
+}
